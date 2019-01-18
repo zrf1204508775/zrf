d9f9617d054fa82b7bb77789c2dfe2432c4b9945
zrf1204508775 <18021068971m0@sina.cn>
2019-01-18 16:09:55 +0800

    first commit

---
+++ page/content.php
@@ -0,0 +1,19 @@
+
+<?php 
+	require  '../components/head.html'; 
+?>
+<title>content</title>
+
+</head>
+<body>
+
+
+<div class="main">
+	<h1>content</h1>
+	<a href="index.php">index</a>
+</div>
+
+
+<?php
+	require  '../components/footer.html';
+?>
---
+++ page/index.php
@@ -0,0 +1,19 @@
+
+<?php 
+	require  '../components/head.html'; 
+?>
+<title>index</title>
+
+</head>
+<body>
+
+
+<div class="main">
+	<h1>index</h1>
+	<a href="content.php">content</a>
+</div>
+
+
+<?php
+	require  '../components/footer.html';
+?>
